3fb905e450e6cf7aab560782ca29338657dadd08
ViktoriiaBlyshchyk <viktoriia.blyshchyk.ik.2021@lpnu.ua>
2021-12-01 18:25:42 +0200

    1

--- PR13.1/PR13.1.cpp
+++ PR13.1/PR13.1.cpp
@@ -1,6 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////
-// PR13.1.cpp
-
 #include <iostream>
 #include <math.h>
 #include <iomanip>
@@ -46,80 +43,4 @@ int main()
 	}
 	cin.get();
 	return 0;
-}
-
-//////////////////////////////////////////////////////////////////////////////
-// var.h
-
-#pragma once
-namespace nsVar 
-{
-	extern int n; 
-	extern double x, x_p, x_k, dx, e, a, s;
-};
-
-//////////////////////////////////////////////////////////////////////////////
-// var.cpp
-
-namespace nsVar
-{ 
-	int n; 
-	double x, x_p, x_k, dx, e, a, s;
-};
-
-//////////////////////////////////////////////////////////////////////////////
-// dod.h
-
-#pragma once
-namespace nsDod 
-{
-	void dod();
-};
-
-//////////////////////////////////////////////////////////////////////////////
-// dod.cpp
-
-#include "dod.h"
-#include "var.h" 
-
-using namespace nsVar;
-
-void nsDod::dod() 
-{
-	a *= (2 * n - 1 * 1.) / ((x * x) * (2 * n + 1)); 
-} 
-
-//////////////////////////////////////////////////////////////////////////////
-// sum.h
-
-#pragma once
-namespace nsSum 
-{
-	void sum();
-};
-
-//////////////////////////////////////////////////////////////////////////////
-// sum.cpp
-
-#include <math.h>
-
-#include "dod.h"
-#include "sum.h"
-#include "var.h" 
-
-using namespace nsDod;
-using namespace nsVar;
-
-void nsSum::sum() 
-{
-	n = 0;
-	a = 1. / x;
-	s = a;
-	do 
-	{
-		n++;
-		dod(); 
-		s += a;
-	} 
-	while (fabs(a) > e);
 }
---
+++ PR13.1/dod.cpp
@@ -0,0 +1,9 @@
+#include "dod.h"
+#include "var.h" 
+
+using namespace nsVar;
+
+void nsDod::dod()
+{
+	a *= (2 * n - 1 * 1.) / ((x * x) * (2 * n + 1));
+}
